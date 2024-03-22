415ea74e1cdb327f69c541c0153f72720e7b16ee
TurkeyChans <louis.mercado192@gmail.com>
2024-03-22 17:38:25 -0400

    Created Post component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Post from './components/Post'; 
+function App() {
+  return (
+    <div>
+      <h1>Fakebook!</h1>
+      
+      <Post /> 
+    </div>
+  );
+}
+
+export default App;
